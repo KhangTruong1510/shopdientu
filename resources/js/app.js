a4437044e222032a93ffc99a496eb81587ddb481
Bao <tran97353@gmail.com>
2022-10-24 22:24:23 +0700

    trang san pham

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,31 @@
 require('./bootstrap');
+//san pham//
+const anhto = document.querySelector(".sanpham-content-left-anhto img")
+const anhnho = document.querySelectorAll(".sanpham-content-left-anhnho img")
+anhnho.forEach(function(imgitem, X) {
+    imgitem.addEventListener("click", function() {
+        anhto.src = imgitem.src
+    })
+})
+
+
+const thongtin = document.querySelector(".thongtin")
+const thongso = document.querySelector(".thongso")
+if (thongso) {
+    thongso.addEventListener("click", function() {
+        document.querySelector(".sanpham-content-right-botom-content-thongtin").style.display = "none"
+        document.querySelector(".sanpham-content-right-botom-content-thongso").style.display = "block"
+    })
+}
+if (thongtin) {
+    thongtin.addEventListener("click", function() {
+        document.querySelector(".sanpham-content-right-botom-content-thongtin").style.display = "block"
+        document.querySelector(".sanpham-content-right-botom-content-thongso").style.display = "none"
+    })
+}
+const butTon = document.querySelector(".sanpham-content-right-botom-top")
+if (butTon) {
+    butTon.addEventListener("click", function() {
+        document.querySelector(".sanpham-content-right-botom-content-cmt").classList.toggle("active")
+    })
+}
